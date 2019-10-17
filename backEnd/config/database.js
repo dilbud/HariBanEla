c38e600b641c62ef83f89dda61aaf1e6b085f6a9
Chamath <e.chamath@yahoo.com>
2019-10-17 17:20:54 +0530

    Initial setup

---
+++ backEnd/config/database.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+exports.connection = function () {
+  mongoose.connect('mongodb+srv://dbAdmin:2fYaO7Ovhl2SQmWN@cluster0-ovc3p.mongodb.net/hariBnEla?retryWrites=true', { useNewUrlParser: true })
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
+
+}
+exports.secretOrKey = "*{%4K58#d[3n";
