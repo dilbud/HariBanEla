3d60b75b0dad0f993f67bd06265d2ca04485426d
Chamath <e.chamath@yahoo.com>
2019-11-15 12:11:44 +0530

    create catogry

--- backEnd/frontEnd/src/app/app-routing.module.ts
+++ backEnd/frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CategoryCreateComponent} from '@modules/category/category-create/category-create.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admin/category/:id",
+  component:CategoryCreateComponent,
+  // canActivate: [AdminGuard]},
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- backEnd/frontEnd/src/app/app.module.ts
+++ backEnd/frontEnd/src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CoreModule } from '@app/core.module';
 import { SharedModule } from '@shared/shared.module';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryCreateComponent } from './modules/category/category-create/category-create.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CategoryCreateComponent,
+     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     CoreModule,
     SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
